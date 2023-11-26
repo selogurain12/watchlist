0813a2e36924a7611f073c6fc3241d998f10e18f
selogurain12 <lora80123@gmail.com>
2023-11-26 19:26:59 +0100

    feat(watchlist/webui): create home page - #2

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:whashlist/ajoutfilm.dart';
 import 'package:whashlist/ajoutlivre.dart';
 import 'package:whashlist/booklist.dart';
+import 'package:whashlist/connexion.dart';
+import 'package:whashlist/inscription.dart';
 import 'package:whashlist/main.dart';
 
 import 'movielist.dart';
@@ -30,7 +32,7 @@ class MyApp extends StatelessWidget {
 class Accueil extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xFFFCE5CB),
         leading: Padding(
@@ -43,84 +45,174 @@ class Accueil extends StatelessWidget {
         title: Center(
           child: Text('HOME'),
         ),
+actions: [
+          GestureDetector(
+            onTap: () {
+              // Naviguer vers la page souhaitée lorsque l'image est cliquée
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Connexion()),
+              );
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(right: 5.0),
+              child: Image.asset(
+                '../images/2815428.png',
+                width: 80.0,
+              ),
+            ),
+          ),
+        ],
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-           SizedBox(
-              width: 200.0, // Ajustez la largeur du bouton
-              height: 60.0, // Ajustez la hauteur du bouton
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => AjoutFilms()),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  side: BorderSide(color: Colors.blue)
+            Padding(
+              padding: const EdgeInsets.all(10.0), // Espacement autour de la première paire de boutons
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 180.0,
+                      height: 180.0,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => AjoutFilms()),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            side: BorderSide(color: Colors.blue)),
+                        child: Text(
+                          'Ajouter un film',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10.0),
+                    SizedBox(
+                      width: 180.0,
+                      height: 180.0,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => AjoutLivre()),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            side: BorderSide(color: Colors.blue)),
+                        child: Text(
+                          'Ajouter un livre',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                child: Text('Ajouter un film',
-                style: TextStyle(color: Colors.black),),
               ),
             ),
-            SizedBox(height: 20.0),
-            SizedBox(
-              width: 200.0,
-              height: 60.0,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Movielist()),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  side: BorderSide(color: Colors.blue)
+            Padding(
+              padding: const EdgeInsets.all(10.0), // Espacement autour de la deuxième paire de boutons
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 180.0,
+                      height: 180.0,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Movielist()),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            side: BorderSide(color: Colors.blue)),
+                        child: Text(
+                          'Ma Movielist',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10.0),
+                    SizedBox(
+                      width: 180.0,
+                      height: 180.0,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => BookList()),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            side: BorderSide(color: Colors.blue)),
+                        child: Text(
+                          'Ma Booklist',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                child: Text('Ma Movielist',
-                style: TextStyle(color: Colors.black),),
               ),
             ),
-            SizedBox(height: 20.0),
-            SizedBox(
-              width: 200.0,
-              height: 60.0,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => AjoutLivre()),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  side: BorderSide(color: Colors.blue)
+            Padding(
+              padding: const EdgeInsets.all(10.0), // Espacement autour de la deuxième paire de boutons
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 180.0,
+                      height: 180.0,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Movielist()),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            side: BorderSide(color: Colors.blue)),
+                        child: Text(
+                          'Mes filmothèques',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10.0),
+                    SizedBox(
+                      width: 180.0,
+                      height: 180.0,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => BookList()),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            side: BorderSide(color: Colors.blue)),
+                        child: Text(
+                          'Mes bibliothèques',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                child: Text('Ajouter un livre',
-                style: TextStyle(color: Colors.black),),
-              ),
-            ),
-            SizedBox(height: 20.0),
-            SizedBox(
-              width: 200.0,
-              height: 60.0,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => BookList()),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  side: BorderSide(color: Colors.blue)
-                ),
-                child: Text('Ma Booklist',
-                style: TextStyle(color: Colors.black),),
               ),
             ),
           ],
