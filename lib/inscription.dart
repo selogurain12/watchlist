be54125786c6064d9c99c84ee363d0ba010363dc
selogurain12 <lora80123@gmail.com>
2023-11-26 19:28:08 +0100

    feat(watchlist/webui): create connexion page - #9

--- lib/inscription.dart
+++ lib/inscription.dart
@@ -1,59 +1,40 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:whashlist/inscription.dart';
+import 'package:whashlist/main.dart';
 
-import 'main.dart';
-
-class Connexion extends StatelessWidget {
+class Inscription extends StatelessWidget {
   final TextEditingController _nomController = TextEditingController();
   final TextEditingController _prenomController = TextEditingController();
   final TextEditingController _mailController = TextEditingController();
   final TextEditingController _motDePasseController = TextEditingController();
   final TextEditingController _confirmationMotDePasseController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-return Scaffold(
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xFFFCE5CB),
         leading: Padding(
           padding: const EdgeInsets.only(left: 8.0),
           child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Accueil()),
-                  );
-                },
-                child: Image.asset(
-            '../images/home.png',
-            width: 30.0,
-          ),
-        ),
-        ),
-        title: Center(
-          child: Text('BOOKS'),
-        ),
-      actions: [
-          GestureDetector(
             onTap: () {
-              // Naviguer vers la page souhaitée lorsque l'image est cliquée
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => Inscription()),
+                MaterialPageRoute(builder: (context) => Accueil()),
               );
             },
-            child: Padding(
-              padding: const EdgeInsets.only(right: 5.0),
-              child: Image.asset(
-                '../images/2815428.png',
-                width: 80.0,
-              ),
+            child: Image.asset(
+              '../images/home.png',
+              width: 30.0,
             ),
           ),
-        ],
+        ),
+        title: Center(
+          child: Text('INSCRIPTION'),
+        ),
       ),
- body: Center(
+      body: Center(
         child: Container(
           padding: const EdgeInsets.all(20.0),
           color: Colors.grey[200],
@@ -61,6 +42,52 @@ return Scaffold(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               // Chaque champ de saisie est enveloppé dans un Container avec une ombre
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3), // Décalage de l'ombre vers le bas
+                    ),
+                  ],
+                ),
+                child: TextField(
+                  controller: _nomController,
+                  decoration: InputDecoration(
+                    hintText: 'Entrez votre nom...',
+                    labelText: 'Nom',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: TextField(
+                  controller: _prenomController,
+                  decoration: InputDecoration(
+                    hintText: 'Entrez votre prénom...',
+                    labelText: 'Prénom',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0),
               Container(
                 decoration: BoxDecoration(
                   color: Colors.white,
@@ -108,19 +135,26 @@ return Scaffold(
                 ),
               ),
               SizedBox(height: 20.0),
-              GestureDetector(
-                onTap: () {
-                  // Naviguer vers la page d'inscription lorsque le texte est cliqué
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Inscription()),
-                  );
-                },
-                child: Text(
-                  'Pas inscrit ? Cliquez ici pour vous inscrire',
-                  style: TextStyle(
-                    color: Colors.blue,
-                    decoration: TextDecoration.underline,
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: TextField(
+                  controller: _confirmationMotDePasseController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Entrez à nouveau votre mot de passe...',
+                    labelText: 'Confirmation du mot de passe',
+                    border: OutlineInputBorder(),
                   ),
                 ),
               ),
@@ -128,13 +162,19 @@ return Scaffold(
               ElevatedButton(
                 onPressed: () {
                   // Accéder aux valeurs saisies dans les champs de texte
+                  String nom = _nomController.text;
+                  String prenom = _prenomController.text;
                   String mail = _mailController.text;
                   String motDePasse = _motDePasseController.text;
+                  String confirmationMotDePasse = _confirmationMotDePasseController.text;
 
                   // Faire quelque chose avec ces valeurs (par exemple, valider l'inscription)
                   // Vous pouvez également les imprimer pour le moment
+                  print('Nom: $nom');
+                  print('Prénom: $prenom');
                   print('Mail: $mail');
                   print('Mot de passe: $motDePasse');
+                  print('Confirmation du mot de passe: $confirmationMotDePasse');
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => Accueil()),
