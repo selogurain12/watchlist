e5eecc65f075cf892ede9d3b3b304b4d441de35b
selogurain12 <lora80123@gmail.com>
2024-02-22 17:59:38 +0100

    create book details page (livre) - #8

---
+++ lib/features/book/presentation/pages/detailsbook_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:whashlist/features/book/domain/entities/book_entity.dart';
+import 'package:whashlist/features/book/presentation/widgets/bookdetails.dart';
+
+class DetailsbookScreen extends StatelessWidget {
+  final ApiBookResponseEntity book;
+  const DetailsbookScreen({Key? key, required this.book}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFFCE5CB),
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: GestureDetector(
+                onTap: () {
+                  context.go('/');
+                },
+                child: Image.asset(
+            '../images/home.png',
+            width: 30.0,
+          ),
+        ),
+        ),
+        title: const Center(
+          child: Text('BOOKS'),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {
+              context.go('/login');
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(right: 5.0),
+              child: Image.asset(
+                '../images/2815428.png',
+                width: 80.0,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: DetailBook(book: book)
+    );
+  }
+}
