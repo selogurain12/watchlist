e0fbffeb2fac96f577f68c90c16a3710044b73d3
selogurain12 <lora80123@gmail.com>
2024-02-22 17:55:55 +0100

    update searchmovie usecase

--- lib/features/movie/domain/usecases/searchmovie.dart
+++ lib/features/movie/domain/usecases/searchmovie.dart
@@ -3,12 +3,12 @@ import 'package:whashlist/core/usecases/usecase.dart';
 import 'package:whashlist/features/movie/domain/entities/movie_entity.dart';
 import 'package:whashlist/features/movie/domain/repositories/movie_repository.dart';
 
-class SearchMovieUseCase implements UseCase<DataState<ApiMovieResponseEntity>, ApiMovieRequestEntity> {
+class SearchMovieUseCase implements UseCase<DataState<List<ApiMovieResponseEntity>>, ApiMovieRequestEntity> {
   final ApiMovieRepository repository;
   const SearchMovieUseCase(this.repository);
   
   @override
-  Future<DataState<ApiMovieResponseEntity>> call({ApiMovieRequestEntity? params}) {
+  Future<DataState<List<ApiMovieResponseEntity>>> call({ApiMovieRequestEntity? params}) {
     return repository.searchmovie(
       body: params,
     );
