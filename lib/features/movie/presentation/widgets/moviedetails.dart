2a0162938ec21a0567f1a8fdd7eb1a4fccc73975
selogurain12 <lora80123@gmail.com>
2024-02-26 21:18:13 +0100

    feat(watchlist/webui): update design - #5

--- lib/features/movie/presentation/widgets/moviedetails.dart
+++ lib/features/movie/presentation/widgets/moviedetails.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:whashlist/features/movie/domain/entities/movie_entity.dart'; // Assurez-vous que le chemin est correct
+import 'package:intl/intl.dart';
+import 'package:whashlist/features/movie/domain/entities/movie_entity.dart';
 
 class DetailMovie extends StatefulWidget {
   final ApiMovieResponseEntity movie;
+
   const DetailMovie({Key? key, required this.movie}) : super(key: key);
 
   @override
@@ -10,93 +12,210 @@ class DetailMovie extends StatefulWidget {
 }
 
 class _DetailBookState extends State<DetailMovie> {
- @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text(widget.movie.title ?? 'Détails du film'),
-    ),
-    body: SingleChildScrollView(
-      padding: const EdgeInsets.all(16.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
         children: [
-          if (widget.movie.backdrop_path != null)
-            Image.network(
-              "https://image.tmdb.org/t/p/original/${widget.movie.poster_path}",
-              width: 200,
-              fit: BoxFit.cover,
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                  "https://image.tmdb.org/t/p/original/${widget.movie.backdrop_path}"),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.2),
+                  BlendMode.dstATop,
+                ),
+              ),
             ),
-          const SizedBox(width: 20),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Titre: ${widget.movie.title}',
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  'Genre(s): ${widget.movie.genre_ids?.join(', ') ?? 'Inconnu'}',
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  'Titre original: ${widget.movie.original_title ?? 'Pas de titre original disponible.'}',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                Text(
-                  'Langue originale: ${widget.movie.original_language ?? "Pas de langue originale disponible."}',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                Text(
-                  'Synopsis: ${widget.movie.overview ?? 'Pas de synopsis disponible.'}',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                 Image.network(
-                  "https://image.tmdb.org/t/p/original/${widget.movie.backdrop_path}",
-                  width: 200,
-                  fit: BoxFit.cover,
-                ),
-                Text(
-                  'Date de sortie: ${widget.movie.release_date ?? 'Pas de date de sortie disponible.'}',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                Text(
-                  'Budget: ${widget.movie.budget ?? 'Pas de budget disponible.'}',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                Text(
-                  'Page d\'accueil: ${widget.movie.homepage ?? 'Pas de page d\'accueil disponible.'}',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                Text(
-                  'Nom de la companie de production: ${widget.movie.companiesname ?? 'Pas de nom de la companie de production disponible.'}',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                Text(
-                  'Logo de la companie de production: ${widget.movie.companieslogo ?? 'Pas de logo de la companie de production disponible.'}',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                Text(
-                  'Revenus: ${widget.movie.revenue ?? 'Pas de revenus disponibles.'}',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                Text(
-                  'Durée: ${widget.movie.runtime ?? 'Pas de durée disponibles.'}',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                Text(
-                  'Moyenne de vote: ${widget.movie.vote_average ?? 'Pas de moyenne de vote disponibles.'}',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-              ],
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      if (widget.movie.backdrop_path != null)
+                        Image.network(
+                          "https://image.tmdb.org/t/p/original/${widget.movie.poster_path}",
+                          width: 100,
+                          fit: BoxFit.cover,
+                        ),
+                      const SizedBox(width: 20),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                const Icon(Icons.movie_creation_outlined),
+                                const SizedBox(width: 5),
+                                Flexible(
+                                  child: Text(
+                                    '${widget.movie.title}',
+                                    style: Theme.of(context).textTheme.titleMedium,
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 10,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 10),
+                            Text(
+                              'Titre original: ${widget.movie.original_title ?? 'Pas de titre original disponible.'}',
+                              style: Theme.of(context).textTheme.titleSmall,
+                            ),
+                            const SizedBox(height: 10),
+                            Row(
+                              children: [
+                                const Icon(Icons.record_voice_over_outlined),
+                                const SizedBox(width: 5),
+                                Text(
+                                  'Langue originale: ${widget.movie.original_language}',
+                                  style: Theme.of(context).textTheme.titleSmall,
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 10),
+                            Row(
+                              children: [
+                                const Icon(Icons.calendar_month),
+                                const SizedBox(width: 5),
+                                Text(
+                                  'Date de sortie: ${widget.movie.release_date}',
+                                  style: Theme.of(context).textTheme.titleMedium,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Genre(s): ${widget.movie.genre_ids?.join(', ') ?? 'Inconnu'}',
+                              style: Theme.of(context).textTheme.titleSmall,
+                            ),
+                            const SizedBox(height: 10),
+                            Text(
+                              'Synopsis: ${widget.movie.overview ?? 'Pas de synopsis disponible.'}',
+                              style: Theme.of(context).textTheme.titleMedium,
+                            ),
+                            const SizedBox(height: 10),
+                            Row(
+                              children: [
+                                const Icon(Icons.monetization_on_outlined),
+                                const SizedBox(width: 5),
+                                Text(
+                                  'Budget: ${formatBudget(widget.movie.budget)}',
+                                  style: Theme.of(context).textTheme.titleMedium,
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 10),
+                            Row(
+                              children: [
+                                const Icon(Icons.language),
+                                const SizedBox(width: 5),
+                                Flexible(
+                                  child: Text(
+                                    'Page d\'accueil: ${widget.movie.homepage}',
+                                    style: Theme.of(context).textTheme.bodyMedium,
+                                  ),
+                                )
+                              ],
+                            ),
+                            const SizedBox(height: 10),
+                            Row(
+                              children: [
+                                const Icon(Icons.monetization_on_outlined),
+                                const SizedBox(width: 5),
+                                Text(
+                                  'Revenus: ${formatBudget(widget.movie.revenue)}',
+                                  style: Theme.of(context).textTheme.titleMedium,
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 10),
+                            Row(
+                              children: [
+                                const Icon(Icons.timer),
+                                const SizedBox(width: 5),
+                                Flexible(
+                                  child: Text(
+                                    'Durée: ${formatRuntime(widget.movie.runtime)}',
+                                    style: Theme.of(context).textTheme.bodyMedium,
+                                  ),
+                                )
+                              ],
+                            ),
+                            const SizedBox(height: 10),
+                            Row(
+                              children: [
+                                Text(
+                                  'Moyenne de vote: ',
+                                  style: Theme.of(context).textTheme.bodyMedium,
+                                ),
+                                buildStarRating(widget.movie.vote_average),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ],
       ),
-    ),
-  );
+    );
+  }
+
+  String formatRuntime(int? runtime) {
+    int hours = runtime! ~/ 60;
+    int minutes = runtime % 60;
+    String formattedRuntime = '$hours h ${minutes}min';
+
+    return formattedRuntime;
+  }
+
+  Widget buildStarRating(double? voteAverage) {
+    if (voteAverage == null) return const Text('Pas de moyenne de vote disponibles.');
+    int numberOfStars = (voteAverage / 2).round();
+    List<Widget> stars = List.generate(
+      numberOfStars,
+      (index) => const Icon(Icons.star, color: Colors.amber),
+    );
+
+    return Row(children: stars);
+  }
+
+  String formatRevenue(int? revenue) {
+  if (revenue == null) {
+    return 'Pas de revenus disponibles.';
+  }
+  final formatter = NumberFormat.currency(locale: 'en_US', symbol: '\$');
+  return formatter.format(revenue);
+}
+String formatBudget(int? budget) {
+  if (budget == null) {
+    return 'Pas de revenus disponibles.';
+  }
+  final formatter = NumberFormat.currency(locale: 'en_US', symbol: '\$');
+  return formatter.format(budget);
 }
 }
