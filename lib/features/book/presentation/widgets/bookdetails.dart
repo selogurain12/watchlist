1ce530ae8a92a5b8b40ebc6ed2cf9738a4949788
selogurain12 <lora80123@gmail.com>
2024-02-26 21:13:57 +0100

    feat(watchlist/webui): update design - #5

--- lib/features/book/presentation/widgets/bookdetails.dart
+++ lib/features/book/presentation/widgets/bookdetails.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:whashlist/features/book/domain/entities/book_entity.dart'; // Assurez-vous que le chemin est correct
 
 class DetailBook extends StatefulWidget {
@@ -13,73 +14,134 @@ class _DetailBookState extends State<DetailBook> {
  @override
 Widget build(BuildContext context) {
   return Scaffold(
-    appBar: AppBar(
-      title: Text(widget.book.title ?? 'Détails du livre'),
-    ),
-    body: SingleChildScrollView(
-      padding: const EdgeInsets.all(16.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          if (widget.book.imageLink != null)
-            Image.network(
-              widget.book.imageLink!,
-              width: 200,
+    body: Stack(
+      children: [
+        Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(
+                widget.book.imageLink!
+              ),
               fit: BoxFit.cover,
-            ),
-          const SizedBox(width: 20),
-          Expanded(
+              colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(0.2),
+                BlendMode.dstATop,
+              ),
+            )
+          ),
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(16.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  'Titre: ${widget.book.title}',
-                  style: Theme.of(context).textTheme.titleLarge,
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      if (widget.book.imageLink != null)
+                        Image.network(
+                          "${widget.book.imageLink}",
+                          width: 100,
+                          fit: BoxFit.cover,
+                        ),
+                        const SizedBox(width: 20),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  const Icon(Icons.book),
+                                  const SizedBox(width: 5),
+                                  Flexible(
+                                    child: Text(
+                                      'Titre: ${widget.book.title}',
+                                      style: Theme.of(context).textTheme.titleLarge,
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 10,
+                                    )
+                                  )
+                                ],
+                              ),
+                              const SizedBox(height: 10),
+                              Row(
+                                children: [
+                                  const Icon(Icons.person),
+                                  const SizedBox(width: 5),
+                                  Flexible(
+                                    child: Text(
+                                      'Auteur(s): ${widget.book.authors?.join(",")}',
+                                      style: Theme.of(context).textTheme.titleMedium,
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 10,
+                                    )
+                                  )
+                                ],
+                              ),
+                            ]
+                          )
+                        )
+                    ],
                 ),
-                const SizedBox(height: 10),
-                Text(
-                  'Auteur(s): ${widget.book.authors?.join(', ') ?? 'Inconnu'}',
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  'Description: ${widget.book.description ?? 'Pas de description disponible.'}',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                Text(
-                  'Edition: ${widget.book.publisher ?? "Pas d'édition disponible."}',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                Text(
-                  'Date de publication: ${widget.book.publisherDate ?? 'Pas de date de publication disponible.'}',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                Text(
-                  'Code ISBN: ${widget.book.industryIdentifieridentyfier ?? 'Pas de code ISBN disponible.'}',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                Text(
-                  'Nombre de pages: ${widget.book.pageCount ?? 'Pas de nombre de pages disponible.'}',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                Text(
-                  'Categories: ${widget.book.categories ?? 'Pas de categories disponible.'}',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                Text(
-                  'Prix: ${widget.book.retailPriceamount} ${widget.book.retailPricecurrencyCode}',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-                Text(
-                  'Lien vers le livre: ${widget.book.retailPricebuyLink ?? 'Pas de lien vers le livre disponible.'}',
-                  style: Theme.of(context).textTheme.bodyMedium,
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    ),
-  );
-}
+                const SizedBox(height: 20),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children:[
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                              Text(
+                                'Description: ${widget.book.description ?? 'Pas de description disponible.'}',
+                                style: Theme.of(context).textTheme.titleMedium,
+                              ),
+                              const SizedBox(height: 10),
+                              Text(
+                                'Edition: ${widget.book.publisher ?? "Pas d'édition disponible."}',
+                                style: Theme.of(context).textTheme.titleSmall,
+                              ),
+                              const SizedBox(height: 10),
+                              Text(
+                                'Date de publication: ${DateFormat("dd/MM/yyyy").format(DateTime.parse(widget.book.publisherDate!))}',
+                                style: Theme.of(context).textTheme.titleSmall,
+                              ),
+                              const SizedBox(height: 10),
+                              Text(
+                                'Code ISBN: ${widget.book.industryIdentifieridentyfier ?? 'Pas de code ISBN disponible.'}',
+                                style: Theme.of(context).textTheme.bodyMedium,
+                              ),
+                              const SizedBox(height: 10),
+                              Text(
+                                'Nombre de pages: ${widget.book.pageCount ?? 'Pas de nombre de pages disponible.'}',
+                                style: Theme.of(context).textTheme.bodyMedium,
+                              ),
+                              const SizedBox(height: 10),
+                              Text(
+                                'Categories: ${widget.book.categories?.join(",")}',
+                                style: Theme.of(context).textTheme.bodyMedium,
+                              ),
+                              const SizedBox(height: 10),
+                              Text(
+                                'Prix: ${widget.book.retailPriceamount} ${widget.book.retailPricecurrencyCode}',
+                                style: Theme.of(context).textTheme.bodyMedium,
+                              ),
+                              const SizedBox(height: 10),
+                              Text(
+                                'Lien vers le livre: ${widget.book.retailPricebuyLink ?? 'Pas de lien vers le livre disponible.'}',
+                                style: Theme.of(context).textTheme.bodyMedium,
+                              ),
+                            ]
+                          )
+                        )
+                      ],
+                    ),
+                  ],
+                )
+              ),
+            )
+          ]
+        )
+      );
+  }
 }
