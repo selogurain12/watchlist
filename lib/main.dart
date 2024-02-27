c673dbc0471494b41d5b32b23f4e1fe9912c4ace
selogurain12 <lora80123@gmail.com>
2024-02-27 16:00:29 +0100

    feat(watchlist/webui): add userProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
 import 'package:whashlist/features/book/presentation/bloc/book_bloc.dart';
 import 'package:whashlist/features/user/presentation/bloc/user_bloc.dart';
+import 'package:whashlist/features/user/presentation/bloc/user_state.dart';
 import 'package:whashlist/injection_container.dart';
 import 'config/routes/app_routes.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDependencies();
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+      create: (context) => UserProvider(),
+      child: MyApp(),
+    ),);
 }
 
 class MyApp extends StatelessWidget {
