769934b08c864984dd3f1536ac90d20cdc2882cd
selogurain12 <lora80123@gmail.com>
2024-02-28 10:31:41 +0100

    feat(watchlist/webui): create connected page

---
+++ lib/features/user/presentation/widgets/connected_body.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:whashlist/features/user/presentation/bloc/user_state.dart';
+
+class ConnectedBody extends StatelessWidget {
+  const ConnectedBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthProvider>(context);
+    final userProvider = Provider.of<UserProvider>(context);
+
+    if (authProvider.isLoggedIn) {
+      Future.delayed(const Duration(seconds: 7), () {
+        context.go('/');
+      });
+
+      return Center(
+        child: Text(
+          'Bon retour ${userProvider.userNom}',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+      );
+    } else {
+      return Container();
+    }
+  }
+}
