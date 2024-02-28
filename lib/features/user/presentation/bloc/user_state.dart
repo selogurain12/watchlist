d666a82e0249ac688725e98720b8ed079a639b1f
selogurain12 <lora80123@gmail.com>
2024-02-28 10:30:37 +0100

    feat(watchlist/webui): create authProvider

--- lib/features/user/presentation/bloc/user_state.dart
+++ lib/features/user/presentation/bloc/user_state.dart
@@ -52,3 +52,19 @@ class UserProvider with ChangeNotifier {
     notifyListeners();
   }
 }
+
+class AuthProvider extends ChangeNotifier {
+  bool _isLoggedIn = false;
+
+  bool get isLoggedIn => _isLoggedIn;
+
+  void login() {
+    _isLoggedIn = true;
+    notifyListeners();
+  }
+
+  void logout() {
+    _isLoggedIn = false;
+    notifyListeners();
+  }
+}
