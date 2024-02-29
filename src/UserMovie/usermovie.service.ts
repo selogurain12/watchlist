2dac3855cb1cf982a0f45dab5c55cc11e52c0b87
selogurain12 <lora80123@gmail.com>
2024-02-29 16:12:54 +0100

    feat(watchlist/back): create user movie entity, module, service and controller

---
+++ src/UserMovie/usermovie.service.ts
@@ -0,0 +1,62 @@
+import { ConflictException, Injectable, NotFoundException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { In, QueryFailedError, Repository } from "typeorm";
+import { FilmFilmotheque, Filmotheque, searchfilmoDto } from "./usermovie.entity";
+import { Movie } from "../Movie/movie.entity";
+import { MovieService } from "../Movie/movie.service";
+
+@Injectable()
+export class UserMovieService {
+  constructor(
+    @InjectRepository(Filmotheque)
+    private readonly filmothequeRepository: Repository<Filmotheque>,
+    @InjectRepository(FilmFilmotheque)
+    private readonly filmfilmothequeRepository: Repository<FilmFilmotheque>,
+    @InjectRepository(Movie)
+    private readonly movieRepository: Repository<Movie>,
+    private readonly movieService: MovieService,
+  ) {}
+    async createfilmotheque(filmo: Filmotheque): Promise<Filmotheque> {
+        try {
+        const newFilmotheque = this.filmothequeRepository.create(filmo);
+      const filmotheque = await this.filmothequeRepository.save(newFilmotheque);
+      return filmotheque;
+    } catch (error) {
+        if (error instanceof QueryFailedError && error.message.includes('duplicate key')) {
+          throw new ConflictException('Il y a déjà une filmothèque avec ce nom');
+        }
+        throw error;
+      }
+    }
+
+    async filmotheque(id: searchfilmoDto): Promise<Filmotheque[]> {
+    return await this.filmothequeRepository.find({where: {
+            id_user: id.id
+    }})
+    }
+
+    async addmovie(movie: FilmFilmotheque): Promise<FilmFilmotheque> {
+        const newmovie =  this.filmfilmothequeRepository.create(movie);
+        return await this.filmfilmothequeRepository.save(newmovie);
+    }
+
+    // Assuming 'id' is of type 'number' in both FilmFilmotheque and Movie entities
+
+    async listmovie(filmotheque: searchfilmoDto): Promise<[FilmFilmotheque, Movie][]> {
+        const films = await this.filmfilmothequeRepository.find({ where: { id_filmotheque: filmotheque.id } });
+      
+        if (!films || films.length === 0) {
+          throw new NotFoundException('No films found for the given filmotheque ID');
+        }
+      
+        const movieDetails: Promise<[FilmFilmotheque, Movie][]> = Promise.all(
+          films.map(async (film) => {
+            const movie = await this.movieService.getMovie(film.id_film); // Use getMovie from MovieService
+            return [film, movie];
+          })
+        );
+      
+        return movieDetails;
+      }
+      
+  }
