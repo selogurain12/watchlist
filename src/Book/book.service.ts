d5f443fc340bfeebccc8ac3060ae36363c47a06a
selogurain12 <lora80123@gmail.com>
2024-02-29 16:16:06 +0100

    feat(watchlist/back): add new function

--- src/Book/book.service.ts
+++ src/Book/book.service.ts
@@ -6,7 +6,7 @@ import { Book } from './book.entity';
 @Injectable()
 export class BookService {
   async searchbook(q: string): Promise<Book[]> {
-      const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${q}`;
+      const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${q}&maxResults=40`;
       const response = await axios.get(apiUrl);
 
       const books: Book[] = response.data.items.map((item: any) => ({
@@ -27,4 +27,35 @@ export class BookService {
       }));
       return books
   }
+
+  async getBook(id: string): Promise<Book> {
+    try {
+    const apiUrl = `https://www.googleapis.com/books/v1/volumes/${id}`;
+    const response = await axios.get(apiUrl);
+
+    if (!response.data) {
+      throw new Error(`Movie details not found in API response`);
+    }
+
+    const books: Book = {
+      id: response.data.id,
+      title: response.data.volumeInfo.title,
+      authors: response.data.volumeInfo.authors,
+      publisher: response.data.volumeInfo.publisher,
+      imageLink: response.data.volumeInfo.imageLinks?.thumbnail,
+      publisherDate: response.data.volumeInfo.publishedDate,
+      description: response.data.volumeInfo.description,
+      industryIdentifierstype: response.data.volumeInfo.industryIdentifiers?.[0]?.type,
+      industryIdentifieridentyfier: response.data.volumeInfo.industryIdentifiers?.[0]?.identifier,
+      pageCount: response.data.volumeInfo.pageCount,
+      categories: response.data.volumeInfo.categories,
+      retailPriceamount: response.data.saleInfo?.retailPrice?.amount,
+      retailPricecurrencyCode: response.data.saleInfo?.retailPrice?.currencyCode,
+      retailPricebuyLink: response.data.saleInfo?.buyLink,
+    };
+    return books
+  } catch (error) {
+    throw error;
+  }
+}
 }
