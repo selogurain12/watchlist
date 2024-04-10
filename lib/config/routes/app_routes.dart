8c0448d1dbe9bb75394fbfdeda0093d5799b8557
Lora Seguin <lora80123@gmail.com>
2024-04-10 15:43:12 +0200

    feat(watchlist/webui): add new route

--- lib/config/routes/app_routes.dart
+++ lib/config/routes/app_routes.dart
@@ -1,8 +1,9 @@
 import 'package:whashlist/features/book/domain/entities/book_entity.dart';
 import 'package:whashlist/features/book/presentation/pages/detailsbook_screen.dart';
 import 'package:whashlist/features/book/presentation/pages/searchbook_screen.dart';
+import 'package:whashlist/features/filmotheques/domain/entities/filmotheques_entity.dart';
+import 'package:whashlist/features/filmotheques/presentation/pages/listmovie_screen.dart';
 import 'package:whashlist/features/filmotheques/presentation/pages/viewfilmo_screen.dart';
-import 'package:whashlist/features/filmotheques/presentation/widgets/listmoviefilmo.dart';
 import 'package:whashlist/features/movie/domain/entities/searchmovie_entity.dart';
 import 'package:whashlist/features/movie/presentation/pages/searchmovie_screen.dart';
 import 'package:whashlist/features/movie/presentation/pages/detailsmovie_screen.dart';
@@ -69,8 +70,8 @@ final router = GoRouter(
     GoRoute(
       path: '/listefilms',
       builder: ((context, state) {
-        final filmotheques = state.extra as String?;
-        return ListMovieFilmo(filmothequeId: filmotheques);
+        final filmotheques = state.extra as FilmothequesResponseEntity;
+        return ListmovieScreen(filmotheque: filmotheques);
       }) 
     ),
   ],
