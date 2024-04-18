96530f66352747571cd78388395ba4fe7bca4e7c
Lora Seguin <lora80123@gmail.com>
2024-04-18 09:13:36 +0200

    feat(watchlist/webui): create view filmo screen page

---
+++ lib/features/bibliotheques/presentation/pages/viewfilmo_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:whashlist/features/bibliotheques/presentation/widgets/viewbibliotheque.dart';
+import 'package:whashlist/features/filmotheques/presentation/widgets/viewfilmotheque.dart';
+import 'package:whashlist/features/user/presentation/bloc/user_state.dart';
+
+class ViewbiblioScreen extends StatelessWidget {
+  const ViewbiblioScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthProvider>(context);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFFCE5CB),
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: GestureDetector(
+                onTap: () {
+                  context.go('/');
+                },
+                child: Image.asset(
+            'home.png',
+            width: 30.0,
+          ),
+        ),
+        ),
+        title: const Center(
+          child: Text('BIBLIOTHEQUES'),
+        ),
+        actions: [
+          authProvider.isLoggedIn
+              ? GestureDetector(
+                  onTap: () {
+                    context.go('/compte');
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 5.0),
+                    child: Image.asset(
+                      '2815428.png',
+                      width: 80.0,
+                    ),
+                  ),
+                )
+              : GestureDetector(
+                  onTap: () {
+                    context.go('/login');
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 5.0),
+                    child: Image.asset(
+                      '2815428.png',
+                      width: 80.0,
+                    ),
+                  ),
+                )
+        ],
+      ),
+      body: const ViewBibliotheque(),
+    );
+  }
+}
