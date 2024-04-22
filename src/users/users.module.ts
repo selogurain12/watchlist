b96def20dcab246ebfb26d1f65714444aa28ad77
Lora Seguin <lora80123@gmail.com>
2024-04-22 20:16:11 +0200

    feat(watchlist/api): add other imports - #27

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -7,10 +7,16 @@ import { MeService } from '../me/me.service';
 import { Stats } from '../me/entities/me.entity';
 import { FriendslistService } from '../friendslist/friendslist.service';
 import { Friendslist } from '../friendslist/entities/friendslist.entity';
+import { Filmotheque } from '../filmotheque/entities/filmotheque.entity';
+import { FilmothequeService } from '../filmotheque/filmotheque.service';
+import { Bibliotheque } from '../bibliotheque/entities/bibliotheque.entity';
+import { BibliothequeService } from '../bibliotheque/bibliotheque.service';
+import { MovieService } from '../Movie/movie.service';
+import { BookService } from '../Book/book.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Stats, Friendslist])],
+  imports: [TypeOrmModule.forFeature([User, Stats, Friendslist, Filmotheque, Bibliotheque])],
   controllers: [UserController],
-  providers: [UserService, MeService, FriendslistService],
+  providers: [UserService, MeService, FriendslistService, FilmothequeService, BibliothequeService, MovieService, BookService],
 })
 export class UserModule {}
