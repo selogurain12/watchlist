b0fced5230d516c60deeeb39e5cf4f81915bcd99
Lora Seguin <lora80123@gmail.com>
2024-04-22 20:07:50 +0200

    feat(watchlist/api): add all missing import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,44 +1,45 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { Statuser } from './user/user.entity';
 import { BookModule } from './Book/book.module';
-import { MovieModule } from './Movie/movie.module';
-import { UserMovieModule } from './UserMovie/usermovie.module';
-import { FilmFilmotheque, Filmotheque } from './UserMovie/usermovie.entity';
-import { Bibliotheque, LivreBibliotheque } from './UserBook/userbook.entity';
-import { UserBookModule } from './UserBook/userbook.module';
-import { Friendlist } from './Friendlist/friendlist.entity';
-import { FriendlistModule } from './Friendlist/friendlist.module';
+import { MovieModule } from './movie/movie.module';
 import { UserModule } from './users/users.module';
 import { User } from './users/entities/user.entity';
 import { MeModule } from './me/me.module';
 import { Stats } from './me/entities/me.entity';
+import { FriendslistModule } from './friendslist/friendslist.module';
+import { Friendslist } from './friendslist/entities/friendslist.entity';
+import { BibliothequeModule } from './bibliotheque/bibliotheque.module';
+import { FilmothequeModule } from './filmotheque/filmotheque.module';
+import { Filmotheque } from './filmotheque/entities/filmotheque.entity';
+import { Bibliotheque } from './bibliotheque/entities/bibliotheque.entity';
+import { FilmsencoursModule } from './filmsencours/filmsencours.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
+      useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.get<string>('DATABASE_HOST', 'localhost'),
         port: configService.get<number>('DATABASE_PORT', 5432),
         username: configService.get<string>('DATABASE_USERNAME', 'lora'),
         password: configService.get<string>('DATABASE_PASSWORD', 'Juemlochda2003!'),
         database: configService.get<string>('DATABASE_NAME', 'watchlist'),
-        entities: [User, Stats, Statuser, Filmotheque, FilmFilmotheque, Bibliotheque, LivreBibliotheque, Friendlist],
+        entities: [User, Stats, Bibliotheque, Friendslist, Filmotheque],
         synchronize: true,
       }),
       inject: [ConfigService],
     }),
     BookModule,
     MovieModule,
-    UserMovieModule,
-    UserBookModule,
-    FriendlistModule,
     UserModule,
-    MeModule
+    MeModule,
+    FriendslistModule,
+    BibliothequeModule,
+    FilmothequeModule,
+    FilmsencoursModule,
   ],
   providers: [ConfigService],
 })
